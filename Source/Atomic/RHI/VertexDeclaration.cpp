72d5f7a9f02a01b170bb3bbdf52a1a5423f20035
Ruben Gomes <rbnpontes@gmail.com>
2024-03-31 12:27:47 -0300

    fix: remove vertex decl useless code

--- Source/Atomic/RHI/VertexDeclaration.cpp
+++ Source/Atomic/RHI/VertexDeclaration.cpp
@@ -60,46 +60,5 @@ namespace REngine
 			input_element.instance_step_rate = 0;
 			++input_layout_desc_.num_elements;
 		}
-
-		//// Loop through the shader input elements and find the corresponding vertex element
-		//for(uint8_t attr_idx = 0; attr_idx < static_cast<uint8_t>(shader_input_elements.Size()); ++attr_idx)
-		//{
-		//	const auto& shader_element = shader_input_elements[attr_idx];
-		//	auto& input_element = input_layout_desc_.elements[input_layout_desc_.num_elements];
-		//	bool found = false;
-		//	for(uint8_t buffer_idx = 0; buffer_idx < Atomic::MAX_VERTEX_STREAMS; ++buffer_idx)
-		//	{
-		//		const auto vertex_buffer = creation_desc.vertex_buffers[buffer_idx];
-		//		if(!vertex_buffer)
-		//			continue;
-		//		const auto vertex_element = vertex_buffer->GetElement(shader_element.semantic, shader_element.semantic_index);
-		//		if(!vertex_element)
-		//			continue;
-
-		//		input_element.buffer_stride = vertex_buffer->GetVertexSize();
-		//		input_element.buffer_index = buffer_idx;
-		//		input_element.input_index = shader_element.index;
-		//		input_element.element_offset = vertex_element->offset_;
-		//		input_element.element_type = vertex_element->type_;
-		//		input_element.instance_step_rate = vertex_element->perInstance_ ? 1 : 0;
-		//		found = true;
-		//		++input_layout_desc_.num_elements;
-		//	}
-
-		//	if(!found)
-		//	{
-		//		// if corresponding vertex element is not found, use default values
-		//		input_element.buffer_stride = 0;
-		//		input_element.buffer_index = 0;
-		//		input_element.input_index = shader_element.index;
-		//		input_element.element_offset = 0;
-		//		input_element.element_type = shader_element.element_type;
-		//		input_element.instance_step_rate = 0;
-		//		++input_layout_desc_.num_elements;
-		//	}
-		//}
-
-		//// Make sure we have the same number of input elements as the shader
-		//assert(input_layout_desc_.num_elements == shader_input_elements.Size());
 	}
 }
