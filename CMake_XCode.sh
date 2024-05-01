726cc15474171d8e02542a95fadd3cf4fa746f34
Ruben <rpg@MacBook-Pro-de-Ruben.local>
2024-05-01 14:41:00 -0300

    fix: xcode build script

--- CMake_XCode.sh
+++ CMake_XCode.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SOURCE=$(cd ${0%/*}; pwd)
-cmake -E make_directory ../AtomicGameEngine-XCode && cmake -E chdir ../AtomicGameEngine-XCode cmake "$SOURCE" -G Xcode
-echo "XCode project written to ../AtomicGameEngine-XCode"
+cmake -E make_directory ../REngine-XCode && cmake -E chdir ../REngine-XCode cmake "$SOURCE" -G Xcode
+echo "XCode project written to ../REngine-XCode"
