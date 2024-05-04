79143e0e0bc1e9349e16eae6342421778630d645
Ruben <rpg@MacBook-Pro-de-Ruben.local>
2024-05-04 19:27:53 -0300

    feat: add nodejs rewrite package tool

--- CMake_IOS.sh
+++ CMake_IOS.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SOURCE=$(cd ${0%/*}; pwd)
-cmake -E make_directory ../REngine-XCode-IOS && cmake -E chdir ../REngine-XCode-IOS cmake "$SOURCE" -G Xcode -DIOS=ON
+cmake -E make_directory ../REngine-XCode-IOS && cmake -E chdir ../REngine-XCode-IOS cmake "$SOURCE" -G Xcode -DIOS=1
 echo "XCode project written to ../REngine-XCode-IOS"
