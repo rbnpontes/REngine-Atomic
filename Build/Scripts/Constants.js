27b2543d80f48a2c19a0806594d9031b911bcfa4
Ruben Gomes <rbnpontes@gmail.com>
2024-07-02 09:20:20 -0300

    chore: change engine default icons

--- Build/Scripts/Constants.js
+++ Build/Scripts/Constants.js
@@ -1,11 +1,11 @@
 module.exports = {
     engine_name : 'REngine',
-    engine_editor_name: 'AtomicEditor',
-    engine_tool_name: 'AtomicTool',
-    engine_player_name: 'AtomicPlayer',
-    engine_net_name : 'AtomicNET',
-    engine_solution_name: 'Atomic.sln',
-    engine_native_lib: 'AtomicNETNative',
-    engine_project_json: 'AtomicProject.json',
+    engine_editor_name: 'EngineEditor',
+    engine_tool_name: 'EngineTool',
+    engine_player_name: 'EnginePlayer',
+    engine_net_name : 'EngineNET',
+    engine_solution_name: 'Engine.sln',
+    engine_native_lib: 'EngineNETNative',
+    engine_project_json: 'EngineProject.json',
     cmake_option_prefix: 'RENGINE'
 };
