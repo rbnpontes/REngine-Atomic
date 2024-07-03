8c43e5ad63bbacc9d63104b37222783374fb221b
Ruben Gomes <rbnpontes@gmail.com>
2024-07-03 16:44:28 -0300

    fix: build issue at visual studio

--- Build/CMake/Modules/EngineWindows.cmake
+++ Build/CMake/Modules/EngineWindows.cmake
@@ -6,7 +6,8 @@ add_definitions(-D_CRT_SECURE_NO_WARNINGS)
 
 # compile with static runtime
 if (MSVC)
-    msvc_set_runtime("/MT")
+    set(ENGINE_MSVC_RUNTIME "/MT")
+    msvc_set_runtime(${ENGINE_MSVC_RUNTIME})
 else ()
     set (ENGINE_WEBVIEW 0)
 endif ()
