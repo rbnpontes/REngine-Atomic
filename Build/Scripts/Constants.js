bfe110f2f7013d41bb163fa90c5aa99fc3ab2ba2
Ruben Gomes <rbnpontes@gmail.com>
2024-07-15 15:10:27 -0300

    refactor: changed atomic player to engine player

--- Build/Scripts/Constants.js
+++ Build/Scripts/Constants.js
@@ -2,7 +2,7 @@ module.exports = {
     engine_name : 'REngine',
     engine_editor_name: 'EngineEditor',
     engine_tool_name: 'EngineTool',
-    engine_player_name: 'AtomicPlayer',
+    engine_player_name: 'EnginePlayer',
     engine_net_name : 'AtomicNET',
     engine_solution_name: 'REngine.sln',
     engine_native_lib: 'AtomicNETNative',
