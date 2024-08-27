b4d11944bedb108c4ef54997b3a6bfa1740636ea
Ruben Gomes <rbnpontes@gmail.com>
2024-08-27 20:36:08 -0300

    feat: add load from executable directory

--- Source/EngineCore/Core/PluginSystem.cpp
+++ Source/EngineCore/Core/PluginSystem.cpp
@@ -6,8 +6,10 @@
 
 #include <SDL2/SDL.h>
 
+#include "IO/FileSystem.h"
+
 namespace REngine {
-    typedef ea::shared_ptr<IEnginePlugin>(*plugin_entrypoint_fn)();
+    typedef void*(*plugin_entrypoint_fn)();
 
 	PluginSystem::PluginSystem(Context* context) : Object(context),
         init_(false),
@@ -15,12 +17,12 @@ namespace REngine {
     {
     }
 
-	ea::vector<ea::shared_ptr<IEnginePlugin>> PluginSystem::GetPlugins() const
+	ea::vector<IEnginePlugin*> PluginSystem::GetPlugins() const
 	{
-        ea::vector<ea::shared_ptr<IEnginePlugin>> result(plugins_.size());
+        ea::vector<IEnginePlugin*> result(plugins_.size());
         u32 i = 0;
         for (const auto& it : plugins_)
-            result[++i] = it.second->instance;
+            result[++i] = it.second->instance_;
         return result;
 	}
 
@@ -34,30 +36,47 @@ namespace REngine {
 
         resource_cache_ = GetSubsystem<ResourceCache>();
 
-        ea::vector<ea::shared_ptr<REngine::IEnginePlugin>> created_plugins;
-        while(!plugins_2_load_.empty())
-        {
-            const auto plugin_path = plugins_2_load_.top();
-            plugins_2_load_.pop();
-
-            const auto plugin = LoadPluginAtPath(plugin_path);
-            created_plugins.push_back(plugin);
-        }
-
-        for (const auto& plugin : created_plugins)
-            plugin->OnPluginSetup(context_);
-        for (const auto& plugin : created_plugins)
-            plugin->OnPluginStart(context_);
+        for (const auto& it : plugins_)
+            it.second->instance_->OnPluginSetup(context_);
+        for (const auto& it : plugins_)
+            it.second->instance_->OnPluginStart(context_);
 
         SubscribeToEvent(E_ENGINE_EXIT, ATOMIC_HANDLER(PluginSystem, HandleEngineExit));
         init_ = true;
+    }
+
+    SharedPtr<File> PluginSystem::TryOpenLib(ea::string lib_path)
+    {
+        #ifdef ENGINE_PLATFORM_WINDOWS
+            if (!string_ends_with(lib_path, ".dll"))
+                lib_path += ".dll";
+        #elif ENGINE_PLATFORM_LINUX|| ENGINE_PLATFORM_ANDROID
+            if (!string_ends_with(lib_path, ".so"))
+                lib_path += ".so";
+        #elif ENGINE_PLATFORM_MACOS || ENGINE_PLATFORM_IOS
+            if (!string_ends_with(lib_path, ".dylib"))
+                lib_path += ".dylib";
+        #endif
+
+        auto file = resource_cache_->GetFile(lib_path.c_str());
+        if (file)
+            return file;
+
+        // try to load shared lib from program directory.
+        lib_path = ToString("%s/%s", GetSubsystem<FileSystem>()->GetProgramDir().CString(), lib_path.c_str()).CString();
+
+        file = resource_cache_->GetFile(lib_path.c_str());
+        if(!file)
+            ATOMIC_LOGWARNINGF("Failed to load plugin: %s", lib_path.c_str());
+
+        return file;
     }
 
     void PluginSystem::HandleEngineExit(StringHash event_type, VariantMap& event_data)
     {
 	    for (const auto& it : plugins_)
 	    {
-            const auto plugin = it.second->instance;
+            const auto plugin = it.second->instance_;
             plugin->OnPluginUnload(context_);
             ATOMIC_LOGDEBUGF("Unload Plugin %d", plugin->GetPluginId());
 	    }
@@ -65,31 +84,17 @@ namespace REngine {
         plugins_.clear(true);
     }
 
-    ea::shared_ptr<REngine::IEnginePlugin> PluginSystem::LoadPluginAtPath(const ea::string& plugin_path)
+    ea::shared_ptr<PluginEntry> PluginSystem::LoadPluginAtPath(const ea::string& plugin_path)
     {
-        ea::string lib_path = plugin_path;
-#ifdef ENGINE_PLATFORM_WINDOWS
-        if (!string_ends_with(lib_path, ".dll"))
-            lib_path += ".dll";
-#elif ENGINE_PLATFORM_LINUX|| ENGINE_PLATFORM_ANDROID
-        if (!string_ends_with(lib_path, ".so"))
-            lib_path += ".so";
-#elif ENGINE_PLATFORM_MACOS || ENGINE_PLATFORM_IOS
-        if (!string_ends_with(lib_path, ".dylib"))
-            lib_path += ".dylib";
-#endif
-
-        auto file = resource_cache_->GetFile(plugin_path.c_str());
+	    const auto file = TryOpenLib(plugin_path);
         if(!file)
         {
             ATOMIC_LOGWARNINGF("Failed to load plugin: %s", plugin_path.c_str());
             return {};
         }
 
-		lib_path = file->GetFullPath().CString();
-
         ATOMIC_LOGDEBUGF("Loading Plugin: %s", plugin_path.c_str());
-        auto lib = SDL_LoadObject(lib_path.c_str());
+        const auto lib = SDL_LoadObject(file->GetFullPath().CString());
         if(!lib)
         {
             ATOMIC_LOGERRORF("Failed to load plugin: %s", plugin_path.c_str());
@@ -105,34 +110,40 @@ namespace REngine {
             return {};
         }
 
-        const auto plugin_instance = plugin_entrypoint();
-        if(plugins_.find_as(plugin_instance->GetPluginId()) == plugins_.end())
+        const auto plugin_instance = static_cast<IEnginePlugin*>(plugin_entrypoint());
+        const auto plugin_it = plugins_.find_as(plugin_instance->GetPluginId());
+        if(plugin_it != plugins_.end())
         {
             ATOMIC_LOGERRORF("Plugin %s is already loaded.", plugin_path.c_str());
             SDL_UnloadObject(lib);
-            return {};
+            return plugin_it->second;
         }
 
         plugin_instance->OnPluginLoad(context_);
 
         const ea::shared_ptr<PluginEntry> entry(new PluginEntry());
-        entry->handle = lib;
-        entry->instance = plugin_entrypoint();
-        plugins_[entry->instance->GetPluginId()] = entry;
-        return entry->instance;
+        entry->handle_ = lib;
+        entry->instance_ = plugin_instance;
+        entry->loaded_ = false;
+        plugins_[entry->instance_->GetPluginId()] = entry;
+        return entry;
     }
 
-    void PluginSystem::LoadPlugin(const ea::string& plugin_path)
+    IEnginePlugin* PluginSystem::LoadPlugin(const ea::string& plugin_path)
     {
-        if (init_)
-        {
-	        const auto plugin = LoadPluginAtPath(plugin_path);
-            plugin->OnPluginSetup(context_);
-            plugin->OnPluginStart(context_);
-            return;
-        }
+	    const auto plugin = LoadPluginAtPath(plugin_path);
+        if (!plugin)
+            return nullptr;
 
-        plugins_2_load_.emplace_back(plugin_path);
+        if (!init_)
+            return plugin->instance_;
+
+        if(!plugin->loaded_)
+        {
+            plugin->instance_->OnPluginSetup(context_);
+            plugin->instance_->OnPluginStart(context_);
+        }
+        return plugin->instance_;
     }
 
     void PluginSystem::UnloadPlugin(u32 plugin_id)
@@ -141,8 +152,8 @@ namespace REngine {
         if(it == plugins_.end())
             return;
 
-        it->second->instance->OnPluginUnload(context_);
-        ATOMIC_LOGDEBUGF("Unload Plugin %d", it->second->instance->GetPluginId());
+        it->second->instance_->OnPluginUnload(context_);
+        ATOMIC_LOGDEBUGF("Unload Plugin %d", it->second->instance_->GetPluginId());
 
 		plugins_.erase(it);
     }
