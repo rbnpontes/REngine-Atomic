d4f265aea0f5cb5a770b24cffdb6fb8c555c87fe
Ruben Gomes <rbnpontes@gmail.com>
2024-09-09 12:44:29 -0300

    feat: update sandbox script

--- Source/Tools/JavaScriptSandbox/TestScripts/sandbox.test.js
+++ Source/Tools/JavaScriptSandbox/TestScripts/sandbox.test.js
@@ -1,5 +1,4 @@
 console.log('.:: Running Engine Sandbox Javascript Tests ::.');
-
 console.log('Running Log Tests');
 console.log('test log info');
 console.debug('test debug log');
@@ -7,16 +6,18 @@ console.warn('test warning log');
 console.error('test error log');
 
 const reqFn = require;
-if(typeof reqFn == 'undefined')
+if (typeof reqFn == 'undefined')
     throw new Error('Require function is not available.');
-if(typeof reqFn.cache == 'undefined')
+if (typeof reqFn.cache == 'undefined')
     throw new Error('Require function doesn\'t contains cache object.');
 
 console.log('Loading test module');
 const moduleExport = require('./test-module.js');
 const moduleResult = moduleExport.result;
-console.log('Module Export: '+moduleResult);
+console.log('Module Export: ' + moduleResult);
 
 const moduleExport2 = require('./test-module.js');
-if(moduleExport2 != moduleExport)
+if (moduleExport2 != moduleExport)
     throw new Error('Invalid require implementation. Require must return same reference of previous loaded module!');
+
+console.log('Require Cache:', reqFn.cache);
