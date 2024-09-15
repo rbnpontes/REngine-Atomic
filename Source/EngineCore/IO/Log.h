4e36c0f9cec26a79c079b5a489fa32b86fdc4b61
Ruben Gomes <rbnpontes@gmail.com>
2024-09-15 16:50:10 -0300

    feat: add log colors and success implementation

--- Source/EngineCore/IO/Log.h
+++ Source/EngineCore/IO/Log.h
@@ -30,135 +30,142 @@
 namespace Atomic
 {
 
-/// Fictional message level to indicate a stored raw message.
-static const int LOG_RAW = -1;
-/// Debug message level. By default only shown in debug mode.
-static const int LOG_DEBUG = 0;
-/// Informative message level.
-static const int LOG_INFO = 1;
-/// Warning message level.
-static const int LOG_WARNING = 2;
-/// Error message level.
-static const int LOG_ERROR = 3;
-/// Disable all log messages.
-static const int LOG_NONE = 4;
+	/// Fictional message level to indicate a stored raw message.
+	static const int LOG_RAW = -1;
+	/// Debug message level. By default only shown in debug mode.
+	static const int LOG_DEBUG = 0;
+	/// Informative message level.
+	static const int LOG_INFO = 1;
+	/// Warning message level.
+	static const int LOG_WARNING = 2;
+	/// Error message level.
+	static const int LOG_ERROR = 3;
+	/// Success message level
+	static const int LOG_SUCCESS = 4;
 
-class File;
+	/// Disable all log messages.
+	static const int LOG_NONE = 5;
 
-/// Stored log message from another thread.
-struct StoredLogMessage
-{
-    /// Construct undefined.
-    StoredLogMessage()
-    {
-    }
+	class File;
 
-    /// Construct with parameters.
-    StoredLogMessage(const String& message, int level, bool error) :
-        message_(message),
-        level_(level),
-        error_(error)
-    {
-    }
+	/// Stored log message from another thread.
+	struct StoredLogMessage
+	{
+		/// Construct undefined.
+		StoredLogMessage()
+		{
+		}
 
-    /// Message text.
-    String message_;
-    /// Message level. -1 for raw messages.
-    int level_;
-    /// Error flag for raw messages.
-    bool error_;
-};
+		/// Construct with parameters.
+		StoredLogMessage(const String& message, int level, bool error) :
+			message_(message),
+			level_(level),
+			error_(error)
+		{
+		}
 
-/// Logging subsystem.
-class ATOMIC_API Log : public Object
-{
-    ATOMIC_OBJECT(Log, Object);
+		/// Message text.
+		String message_;
+		/// Message level. -1 for raw messages.
+		int level_;
+		/// Error flag for raw messages.
+		bool error_;
+	};
 
-public:
-    /// Construct.
-    Log(Context* context);
-    /// Destruct. Close the log file if open.
-    virtual ~Log();
+	/// Logging subsystem.
+	class ATOMIC_API Log : public Object
+	{
+		ATOMIC_OBJECT(Log, Object);
 
-    /// Open the log file.
-    void Open(const String& fileName);
-    /// Close the log file.
-    void Close();
-    /// Set logging level.
-    void SetLevel(int level);
-    /// Set whether to timestamp log messages.
-    void SetTimeStamp(bool enable);
-    /// Set quiet mode ie. only print error entries to standard error stream (which is normally redirected to console also). Output to log file is not affected by this mode.
-    void SetQuiet(bool quiet);
+	public:
+		/// Construct.
+		Log(Context* context);
+		/// Destruct. Close the log file if open.
+		virtual ~Log();
 
-    /// Return logging level.
-    int GetLevel() const { return level_; }
+		/// Open the log file.
+		void Open(const String& fileName);
+		/// Close the log file.
+		void Close();
+		/// Set logging level.
+		void SetLevel(int level);
+		/// Set whether to timestamp log messages.
+		void SetTimeStamp(bool enable);
+		/// Set quiet mode ie. only print error entries to standard error stream (which is normally redirected to console also). Output to log file is not affected by this mode.
+		void SetQuiet(bool quiet);
 
-    /// Return whether log messages are timestamped.
-    bool GetTimeStamp() const { return timeStamp_; }
+		/// Return logging level.
+		int GetLevel() const { return level_; }
 
-    /// Return last log message.
-    String GetLastMessage() const { return lastMessage_; }
+		/// Return whether log messages are timestamped.
+		bool GetTimeStamp() const { return timeStamp_; }
 
-    /// Return whether log is in quiet mode (only errors printed to standard error stream).
-    bool IsQuiet() const { return quiet_; }
+		/// Return last log message.
+		String GetLastMessage() const { return lastMessage_; }
 
-    /// Write to the log. If logging level is higher than the level of the message, the message is ignored.
-    static void Write(int level, const String& message);
-    /// Write raw output to the log.
-    static void WriteRaw(const String& message, bool error = false);
+		/// Return whether log is in quiet mode (only errors printed to standard error stream).
+		bool IsQuiet() const { return quiet_; }
 
-    // ATOMIC BEGIN
+		/// Write to the log. If logging level is higher than the level of the message, the message is ignored.
+		static void Write(int level, const String& message);
+		/// Write raw output to the log.
+		static void WriteRaw(const String& message, bool error = false);
 
-    const File* GetLogFile() const { return logFile_; }
+		// ATOMIC BEGIN
 
-    // ATOMIC END
+		const File* GetLogFile() const { return logFile_; }
 
-private:
-    /// Handle end of frame. Process the threaded log messages.
-    void HandleEndFrame(StringHash eventType, VariantMap& eventData);
-    /// Execute Stored Log Message.
-    void DigestStoredLog(const StoredLogMessage& log_msg);
+		// ATOMIC END
 
-    /// Mutex for threaded operation.
-    Mutex logMutex_;
-    /// Log messages from other threads.
-    List<StoredLogMessage> threadMessages_;
-    /// Log file.
-    SharedPtr<File> logFile_;
-    /// Last log message.
-    String lastMessage_;
-    /// Logging level.
-    int level_;
-    /// Timestamp log messages flag.
-    bool timeStamp_;
-    /// In write flag to prevent recursion.
-    bool inWrite_;
-    /// Quiet mode flag.
-    bool quiet_;
-};
+	private:
+		/// Handle end of frame. Process the threaded log messages.
+		void HandleEndFrame(StringHash eventType, VariantMap& eventData);
+		/// Execute Stored Log Message.
+		void DigestStoredLog(const StoredLogMessage& log_msg);
+
+		/// Mutex for threaded operation.
+		Mutex logMutex_;
+		/// Log messages from other threads.
+		List<StoredLogMessage> threadMessages_;
+		/// Log file.
+		SharedPtr<File> logFile_;
+		/// Last log message.
+		String lastMessage_;
+		/// Logging level.
+		int level_;
+		/// Timestamp log messages flag.
+		bool timeStamp_;
+		/// In write flag to prevent recursion.
+		bool inWrite_;
+		/// Quiet mode flag.
+		bool quiet_;
+	};
 
 #ifdef ENGINE_LOGGING
 #define ATOMIC_LOGDEBUG(message) Atomic::Log::Write(Atomic::LOG_DEBUG, message)
 #define ATOMIC_LOGINFO(message) Atomic::Log::Write(Atomic::LOG_INFO, message)
 #define ATOMIC_LOGWARNING(message) Atomic::Log::Write(Atomic::LOG_WARNING, message)
 #define ATOMIC_LOGERROR(message) Atomic::Log::Write(Atomic::LOG_ERROR, message)
+#define ATOMIC_LOGSUCCESS(message) Atomic::Log::Write(Atomic::LOG_SUCCESS, message)
 #define ATOMIC_LOGRAW(message) Atomic::Log::WriteRaw(message)
 #define ATOMIC_LOGDEBUGF(format, ...) Atomic::Log::Write(Atomic::LOG_DEBUG, Atomic::ToString(format, ##__VA_ARGS__))
 #define ATOMIC_LOGINFOF(format, ...) Atomic::Log::Write(Atomic::LOG_INFO, Atomic::ToString(format, ##__VA_ARGS__))
 #define ATOMIC_LOGWARNINGF(format, ...) Atomic::Log::Write(Atomic::LOG_WARNING, Atomic::ToString(format, ##__VA_ARGS__))
 #define ATOMIC_LOGERRORF(format, ...) Atomic::Log::Write(Atomic::LOG_ERROR, Atomic::ToString(format, ##__VA_ARGS__))
+#define ATOMIC_LOGSUCCESSF(format, ...) Atomic::Log::Write(Atomic::LOG_SUCCESS, Atomic::ToString(format, ##__VA_ARGS__))
 #define ATOMIC_LOGRAWF(format, ...) Atomic::Log::WriteRaw(Atomic::ToString(format, ##__VA_ARGS__))
 #else
 #define ATOMIC_LOGDEBUG(message) ((void)0)
 #define ATOMIC_LOGINFO(message) ((void)0)
 #define ATOMIC_LOGWARNING(message) ((void)0)
 #define ATOMIC_LOGERROR(message) ((void)0)
+#define ATOMIC_LOGSUCCESS(message) ((void)0)
 #define ATOMIC_LOGRAW(message) ((void)0)
 #define ATOMIC_LOGDEBUGF(...) ((void)0)
 #define ATOMIC_LOGINFOF(...) ((void)0)
 #define ATOMIC_LOGWARNINGF(...) ((void)0)
 #define ATOMIC_LOGERRORF(...) ((void)0)
+#define ATOMIC_LOGSUCCESSF(...) ((void)0)
 #define ATOMIC_LOGRAWF(...) ((void)0)
 #endif
 
