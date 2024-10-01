529789e89546416d3ce574af5aa2e4c0803af6e0
Ruben Gomes <rbnpontes@gmail.com>
2024-10-01 12:52:33 -0300

    feat: registered js system with interface type

--- Source/Plugins/EngineJS/Sources/JavaScriptSystem.h
+++ Source/Plugins/EngineJS/Sources/JavaScriptSystem.h
@@ -12,16 +12,18 @@ namespace REngine
 		ATOMIC_OBJECT(JavaScriptSystem, Object);
 	public:
 		JavaScriptSystem(Context* context);
-		void Initialize();
+		// begin IJavaScriptSystem overrides
 		bool Eval(const ea::string& js_code) override;
+		bool Eval(SharedPtr<File> script_file) const override;
 		bool EvalFromFilePath(const ea::string& file_path) override;
-		bool Eval(SharedPtr<File> script_file) const;
 		void ExecutePendingTimers() override;
 		void ClearAllTimers() override;
 		bool HasPendingTimers() override;
-		duk_context* GetHeap() const override { return js_context_; }
+		duk_context* GetHeap() const override;
 		size_t GetMemoryUsage() const override { return memory_usage_; }
 		size_t GetMemoryBlocks() const override { return memory_blocks_; }
+		// end IJavaScriptSystem overrides
+		void Initialize();
 		static Context* GetEngineContext(duk_context* ctx);
 	private:
 		void OnBeginFrame(StringHash event_type, VariantMap& event_args);
